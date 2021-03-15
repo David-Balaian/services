3b96592750757e60e07e6801e5fed0efb61edd7f
David-Balaian <davidbalayan1@gmail.com>
2021-03-15 09:37:08 +0400

    services

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (<>
+    <div>Internet providing</div>
+    <div>BGP Service</div>
+    <div>Data providing</div>
+    <div>TV providing</div>
+    <div>Network design, set-up, expertise, optimisation and service</div>
+    <div>Network active equipment installation, configuration and service</div>
+    <div>Network passive equipment installation, configuration and service</div>
+    <div>Data processing center design, set-up, expertise and service</div>
+    <div>IP TV, CATV and OTT design, set-up, expertise, optimisation and service</div>
+    </>
+  );
+}
+
+export default App;
