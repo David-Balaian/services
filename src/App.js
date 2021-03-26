74cf3310d6a2854c82d8524feed129535873e31e
David-Balaian <davidbalayan1@gmail.com>
2021-03-26 22:31:49 +0400

    final

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (<>
-    <div>Internet providing</div>
-    <div>BGP Service</div>
-    <div>Data providing</div>
-    <div>TV providing</div>
-    <div>Network design, set-up, expertise, optimisation and service</div>
-    <div>Network active equipment installation, configuration and service</div>
-    <div>Network passive equipment installation, configuration and service</div>
-    <div>Data processing center design, set-up, expertise and service</div>
-    <div>IP TV, CATV and OTT design, set-up, expertise, optimisation and service</div>
-    </>
+  return (<div className="container">
+    <div className="item" >Internet providing</div>
+    <div className="item" >BGP Service</div>
+    <div className="item" >Data providing</div>
+    <div className="item" >TV providing</div>
+    <div className="item" >Network design, set-up, expertise, optimisation and service</div>
+    <div className="item" >Network active equipment installation, configuration and service</div>
+    <div className="item" >Network passive equipment installation, configuration and service</div>
+    <div className="item" >Data processing center design, set-up, expertise and service</div>
+    <div className="item" >IP TV, CATV and OTT design, set-up, expertise, optimisation and service</div>
+    </div>
   );
 }
 
